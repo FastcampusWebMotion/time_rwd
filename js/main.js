5a482d78c5a6fbae4803696b3958ce6af4ac8c98
tubi55 <tubi55@nate.com>
2022-01-07 20:07:50 +0900

    시간에 따른 반응형

--- js/main.js
+++ js/main.js
@@ -1,21 +1,58 @@
-﻿const time = document.querySelectorAll('.screen span');
+﻿const main = document.querySelector('main');
+const menus = document.querySelectorAll('nav span');
+const numbers = document.querySelectorAll('.screen span');
+const [am, pm] = document.querySelectorAll('.screen em');
 
 setInterval(()=>{
+    let now = new Date();
+    let hr = now.getHours();
+
+    if(hr>=5 && hr<11){
+        main.className='';
+        main.classList.add('morning');
+    }
+    if(hr>=11 && hr<16){
+        main.className='';
+        main.classList.add('afternoon');
+    }
+    if(hr>=16 && hr<19){
+        main.className='';
+        main.classList.add('evening');        
+    }
+    if(hr>=19 || hr<5){
+        main.className='';
+        main.classList.add('night');
+    }
+
+    const times = setTime();
+    times.forEach((time, index)=> getTime(time, index));    
+},1000);
+
+
+function setTime(){
+    let hr2 = null;
     let now = new Date();
     let hr = now.getHours();
     let min = now.getMinutes();
     let sec = now.getSeconds();
 
-    if(hr<10) hr = '0'+hr;
-    if(min<10) min = '0'+min;
-    if(sec<10) sec = '0'+sec;
+    if(hr>12){
+        hr2 = hr-12;
+        pm.classList.add('on');
+        am.classList.remove('on');
+    }else{
+        hr2 = hr;
+        am.classList.add('on');
+        pm.classList.remove('on');
+    }
 
-    time[0].innerText = hr;
-    time[1].innerText = min;
-    time[2].innerText = sec;
-},1000);
+    return [hr2, min, sec];
+}
 
-
+function getTime(num, index){
+    if(num<10) num = '0'+num;
+    numbers[index].innerText = num;
+}
 
 
 
